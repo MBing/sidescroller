75ee476d3e6ed9434df0efd6db67af2a15dba9e5
Jack Doiron <jack83307@gmail.com>
2013-04-13 16:54:58 -0400

    Made sidescroller class and some other fixes

--- js/main.js
+++ js/main.js
@@ -1,77 +1,18 @@
 // This is the game's main JavaScript file
 
+// Declare global variables
+var canvas,
+    ctx,
+    game;
+
 $(document).ready(function() {
 	
-	var canvas = $('#canvas')[0],
-	    ctx = canvas.getContext('2d'),
-	    isRunning,
-	    delayTime,
-	    // Future animation
-	    FPS = delayTime * 1000,
-	    // Atiles for map class
-	    map = [];
-
-	// Animation fix instead of setInterval() for the lack of support of browsers.
-	window.requestAnimFrame = (function(){
-		
-		return  window.requestAnimationFrame       ||
-        	window.webkitRequestAnimationFrame ||
-		window.mozRequestAnimationFrame    ||
-            	function(callback){
-            		
-            		window.setTimeout(callback, delayTime);
-		};
-	})();
+	// Define variables
+	canvas = $('#canvas'),
+	ctx = canvas[0].getContext('2d'),
+	game = new SideScroller(canvas);
 	
-	// Clears the canvas and makes place for a new frame
-	function clear() {
-		
-		context.fillStyle = "white";
-		context.fillRect(0, 0, canvas.width, canvas.height);
-	}
-
-	// Updates the position of the stuff or whatever its gonna do
-	function update() {
-
-	}
-
-	// Renders the new scene
-	function render() {
-		
-	}
-
-	function GameLoop(running) {
-		
-		if ( !running ) {
-			
-			return;
-		}
-		var beforeTime = performance.now();
-		
-		clear();
-		update();
-		render();
-		requestAnimFrame(GameLoop);
-		
-		var afterTime = performance.now()
-		delayTime = afterTime - beforeTime;
-	}	
-
-	function startGame() {
-		
-		isRunning = true;
-		
-		// Call to map class.
-		new Map(tiles, canvas, 0);
-		GameLoop(window.isRunning);
-	}
-	
-	function stopGame() {
-		
-		isRunning = false;
-	}
-	
-	canvas.keyDown(function(event) {
+	canvas.keydown(function(event) {
 		
 		event.preventDefault();
 		var key = event.keyCode();
---
+++ js/sidescroller_class.js
@@ -0,0 +1,96 @@
+// SideScroller object
+
+/***************************
+SideScroller arguments:
+canvas - the canvas to draw on, can be any valid argument for the jQuery object
+***************************/
+function SideScroller(canvas) {
+
+	var delayTime,
+	    // Future animation for browsers that lack requestAnimationFrame
+	    FPS = 60,
+		// amount of time taken to draw last frame
+		delayTime,
+		// canvas to draw on
+		canvas = $(canvas)[0],
+		// canvas context
+		ctx = canvas.getContext("2d"),
+		// If the game is running
+	    running,
+	    // Atiles for map class
+	    tiles = [],
+		// Map class
+		map,
+		// Use to calculate position of elements so the game doesn't run faster on computers with faster framerate
+	    previousFrame;
+		
+	// Animation fix instead of setInterval() for the lack of support of browsers.
+	requestAnimationFrame = (function(){
+		
+		return  window.requestAnimationFrame       ||
+        	window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame    ||
+            	function(callback){
+            		
+            		window.setTimeout(callback, 1000/FPS - delayTime);
+		};
+	})();
+	
+	// Clears the canvas and makes place for a new frame
+	function clear() {
+		
+		context.fillStyle = "white";
+		context.fillRect(0, 0, canvas.width, canvas.height);
+	}
+	
+	// Updates the position of the stuff or whatever its gonna do
+	function update() {
+		
+	}
+	
+	// Renders the new scene
+	function render() {
+		
+	}
+	
+	// Update the game each frame
+	function GameLoop() {
+		
+		// Stop if not running
+		if ( !running ) {
+			return;
+		}
+		
+		console.log("hi");
+		var beforeTime = performance.now();
+		
+		clear();
+		update();
+		render();
+		requestAnimationFrame(GameLoop);
+		
+		previousFrame = beforeTime;
+		var afterTime = performance.now()
+		delayTime = afterTime - beforeTime;
+	}
+	
+	// Start the game
+	this.start = function() {
+		
+		running = true;
+		
+		// Call to map class.
+		map = new Map(tiles, canvas, 0);
+		requestAnimationFrame(GameLoop);
+	}
+	
+	// Stop the game
+	this.stop = function() {
+		running = false;
+	}
+	
+	// Determine if the game is running
+	this.isRunning = function() {
+		return running;
+	}
+}
