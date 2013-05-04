84bf8edfa5349c7174f4ff7823e3ab3abcb90875
Maartenf <maartenforrier@hotmail.com>
2013-05-04 21:24:58 +0300

    IT WORKS!!! :D
    Deleted the requestanim things... We don't need it, it's too complicated.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,64 @@
 // This is the game's main JavaScript file
 
-// Declare global variables
-var canvas,
-    ctx,
-    game;
-
 $(document).ready(function() {
+
+	var canvas = document.getElementById("theCanvas");
+	var ctx = canvas.getContext('2d');
+	    
+	var isRunning = false;
+	var delayTime = 0;
+	// Future animation
+	var FPS = delayTime * 1000;
 	
-	// Define variables
-	canvas = $('#canvas'),
-	ctx = canvas[0].getContext('2d'),
-	game = new SideScroller(canvas);
+	// Tiles for map class
+	var map = [];
 	
-	canvas.keydown(function(event) {
+	// Clears the canvas and makes place for a new frame
+	function clear() {
+		ctx.fillStyle = "white";
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+	}
+
+	// Updates the position of the stuff or whatever it's going to do
+	function update() {
 		
-		event.preventDefault();
-		var key = event.keyCode();
+	}
+
+	// Renders the new scene
+	function render() {
+		ctx.fillStyle = "cyan";
+		ctx.fillRect(0, 0, 40, 40);
+	}
+
+	function GameLoop(running) {
+		setInterval(function() {
+			if (!running) {
+				return;
+			} else {
+				var beforeTime = performance.now();
+				
+				clear();
+				update();
+				render();
+
+				var afterTime = performance.now()
+				delayTime = afterTime - beforeTime;
+			}
+		}, delayTime);
+	}	
+
+	function startGame() {
+		isRunning = true;
 		
-		// ...
-	});
+		// Call to map class.
+		//new Map(tiles, canvas, 0);
+		new GameLoop(isRunning);
+	}
+	
+	function stopGame() {
+		
+		isRunning = false;
+	}
+	
+	startGame();
 });
